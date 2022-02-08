1b73a1f79ab72469df812588608d4d9e8bb4e589
zippen <2836284560@qq.com>
2022-02-09 01:26:58 +0800

    feat(icon): add element-plus/icon

---
+++ src/views/login/index.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    <avatar />
+  </div>
+</template>
+
+<script setup>
+import { Avatar } from '@element-plus/icons'
+</script>
+
+<style lang="scss" scoped>
+svg {
+  width: 2em;
+  height: 2em;
+}
+</style>
