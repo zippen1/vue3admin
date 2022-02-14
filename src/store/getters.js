d2ab0e8b426c8d08101af7da0c249e6c7095f6c4
zippen <2836284560@qq.com>
2022-02-14 16:35:44 +0800

    feat(Avatar): 新增用户头像

--- src/store/getters.js
+++ src/store/getters.js
@@ -2,6 +2,7 @@ const getters = {
   token: (state) => state.user.token,
   hasUserInfo: (state) => {
     return JSON.stringify(state.user.userInfo) !== '{}'
-  }
+  },
+  userInfo: (state) => state.user.userInfo
 }
 export default getters
