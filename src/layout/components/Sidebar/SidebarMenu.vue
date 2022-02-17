956e5cd5d0a2bb166c906973c774e7c0a5cc2d62
zippen <2836284560@qq.com>
2022-02-17 16:24:47 +0800

    feat(menu): 生成动态menu菜单，并修改其外观样式

--- src/layout/components/Sidebar/SidebarMenu.vue
+++ src/layout/components/Sidebar/SidebarMenu.vue
@@ -1,29 +1,40 @@
 <template>
   <!-- 一级 menu 菜单 -->
   <el-menu
-    :uniqueOpened="true"
-    default-active="2"
-    background-color="#545c64"
-    text-color="#fff"
-    active-text-color="#ffd04b"
+    :default-active="activeMenu"
+    :background-color="$store.getters.cssVar.menuBg"
+    :text-color="$store.getters.cssVar.menuText"
+    :active-text-color="$store.getters.cssVar.menuActiveText"
+    :unique-opened="true"
+    router
   >
-    <!-- 子集 menu 菜单 -->
-    <el-submenu index="1">
-      <template #title>
-        <i class="el-icon-location"></i>
-        <span>导航一</span>
-      </template>
-      <el-menu-item index="1-1">选项1</el-menu-item>
-      <el-menu-item index="1-2">选项2</el-menu-item>
-    </el-submenu>
-    <!-- 具体菜单项 -->
-    <el-menu-item index="4">
-      <i class="el-icon-setting"></i>
-      <template #title>导航四</template>
-    </el-menu-item>
+    <sidebar-item
+      v-for="item in routes"
+      :key="item.path"
+      :route="item"
+    ></sidebar-item>
   </el-menu>
 </template>
 
-<script setup></script>
+<script setup>
+import { computed } from 'vue'
+import SidebarItem from './SidebarItem'
+import { useRouter, useRoute } from 'vue-router'
+import { filterRouters, generateMenus } from '@/utils/route'
+
+// 计算路由表结构
+const router = useRouter()
+const routes = computed(() => {
+  const filterRoutes = filterRouters(router.getRoutes())
+  return generateMenus(filterRoutes)
+})
+
+// 计算高亮 menu 的方法
+const route = useRoute()
+const activeMenu = computed(() => {
+  const { path } = route
+  return path
+})
+</script>
 
 <style lang="scss" scoped></style>
--- src/store/getters.js
+++ src/store/getters.js
@@ -1,8 +1,10 @@
+import variables from '@/styles/variables.scss'
 const getters = {
   token: (state) => state.user.token,
   hasUserInfo: (state) => {
     return JSON.stringify(state.user.userInfo) !== '{}'
   },
-  userInfo: (state) => state.user.userInfo
+  userInfo: (state) => state.user.userInfo,
+  cssVar: (state) => variables
 }
 export default getters
