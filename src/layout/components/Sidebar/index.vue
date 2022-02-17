4cc73cd22868ea9d0417b3cb07b45e28e7658423
zippen <2836284560@qq.com>
2022-02-17 08:30:47 +0800

    feat(router&menu): 初始化一个简易的menu菜单，根据业务需求创建必要的view页面，并创建对应的路由

--- src/layout/components/Sidebar/index.vue
+++ src/layout/components/Sidebar/index.vue
@@ -1,7 +1,14 @@
 <template>
-  <div>sidebar</div>
+  <div>
+    <h1>占位</h1>
+    <el-scrollbar>
+      <sidebar-menu></sidebar-menu>
+    </el-scrollbar>
+  </div>
 </template>
 
-<script setup></script>
+<script setup>
+import SidebarMenu from './SidebarMenu'
+</script>
 
 <style lang="scss" scoped></style>
